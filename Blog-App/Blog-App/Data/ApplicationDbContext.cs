28b5fde02cdd47b5b5d42f1d95c0ea4bb10b622c
BhagyaDidulanka <didulankabhagya@gmail.com>
2025-04-17 13:06:12 +0530

    Create DbContext Class

---
+++ Blog-App/Blog-App/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BlogApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog_App.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
--- Blog-App/Blog-App/obj/Debug/net8.0/Blog-App.AssemblyInfo.cs
+++ Blog-App/Blog-App/obj/Debug/net8.0/Blog-App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0f1d2cfa0076216c812bfdf9908284b587362c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+106732243dc473742cf1acf88831d12c752664e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
