210f9e256dd24d4a7fb0a1eb00d239e2cb0a8d9f
BhagyaDidulanka <120791157+BhagyaDidulanka@users.noreply.github.com>
2025-06-09 00:51:35 +0530

    Update IPostRepository.cs

--- Blog-App/Blog-App/Repositories/Interface/IPostRepository.cs
+++ Blog-App/Blog-App/Repositories/Interface/IPostRepository.cs
@@ -1,4 +1,4 @@
-﻿using Blog_App.Dto.Response;
+using Blog_App.Dto.Response;
 using BlogApp.Models;
 
 namespace Blog_App.Repositories.Interface
@@ -6,10 +6,10 @@ namespace Blog_App.Repositories.Interface
     public interface IPostRepository
     {
         Task<PostResponseDto> CreatePost(Post post);
-        Task<List<Post>> GetAllpost();
+        Task<List<Post>> GetAllposts();
         Task<Post> GetPostById(int id);
         Task<Post> Update(Post post);
         Task DeletePost(Post deletedPost);
-        Task<List<Post>> GetAllPosts();
+        //Task<List<Post>> GetAllPosts();
     }
 }
